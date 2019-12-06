71f9410ab50be17a2585b33bb3224baf018cbce6
Thomas Hackl <thomas.hackl@uni-passau.de>
2019-12-06 12:04:36 +0100

    vue component for Stud.IP messageboxes

---
+++ resources/vue/components/Messagebox.vue
@@ -0,0 +1,39 @@
+<template>
+    <div :class="'messagebox messagebox_' + type">
+        <div class="messagebox_buttons">
+            <a class="close" href="#" title="Nachrichtenbox schließen">
+                <span>Nachrichtenbox schließen</span>
+            </a>
+        </div>
+        <div>
+            {{ message }}
+        </div>
+        <div v-if="details.length > 0">
+            <ul>
+                <li v-for="(detail, index) in details" :key="index">
+                    {{ detail }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Messagebox',
+        props: {
+            type: {
+                type: String,
+                default: 'info'
+            },
+            message: {
+                type: String,
+                default: ''
+            },
+            details: {
+                type: Array,
+                default: []
+            }
+        }
+    }
+</script>
